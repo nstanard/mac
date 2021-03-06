fd17e16cb47e8e571f565e911c2283554fb3a345
Neal Stanard <nstanard@gmail.com>
2021-03-06 16:48:47 -0600

    more aliases

--- scripts/devrc.sh
+++ scripts/devrc.sh
@@ -11,6 +11,15 @@ pathmunge "/usr/local/sbin"
 # bash shortcut cheatsheet open with safari
 alias bs="open -a /Applications/Safari.app https://blog.ssdnodes.com/blog/cheatsheet-bash-shortcuts/"
 
+# list aliases
+alias commands="grep -in --color -e '^alias\s+*' ~/.devrc | sed 's/alias //' | grep --color -e ':[a-z][a-z0-9]*'"
+
+# keep mac alive for a certain time
+alias nosleep=caffeinate -t 360000
+
+# count lines of code quickly
+alias sloc="git ls-files \"*.js*\" \"*.scss\" | xargs wc -l"
+
 alias re='sudo !!'
 
 alias h='history'
@@ -34,6 +43,49 @@ alias test="npm run test"
 
 # GIT
 alias push="git push origin HEAD"
+# ----------------------
+# Git Aliases
+# ----------------------
+alias ga='git add'
+alias gaa='git add .'
+alias gaaa='git add --all'
+alias gau='git add --update'
+alias gb='git branch'
+alias gbd='git branch --delete '
+alias gc='git commit'
+alias gcm='git commit --message'
+alias gcf='git commit --fixup'
+alias gco='git checkout'
+alias gcob='git checkout -b'
+alias gcom='git checkout master'
+alias gcos='git checkout staging'
+alias gcod='git checkout develop'
+alias gd='git diff'
+alias gda='git diff HEAD'
+alias gf='git fetch --all'
+alias gi='git init'
+alias glg='git log --graph --oneline --decorate --all'
+alias gld='git log --pretty=format:"%h %ad %s" --date=short --all'
+alias gm='git merge --no-ff'
+alias gma='git merge --abort'
+alias gmc='git merge --continue'
+alias gp='git pull'
+alias gpr='git pull --rebase'
+alias gr='git rebase'
+alias gs='git status'
+alias gss='git status --short'
+alias gst='git stash'
+alias gsta='git stash apply'
+alias gstd='git stash drop'
+alias gstl='git stash list'
+alias gstp='git stash pop'
+alias gsts='git stash save'
+
+# ----------------------
+# Git Functions
+# ----------------------
+# Git log find by commit message
+function glf() { git log --all --grep="$1"; }
 
 EOF
 
