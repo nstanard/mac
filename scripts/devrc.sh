4a2c47d156b0b619e4c1dbab467c2b1ce110d11c
Neal Stanard <nstanard@gmail.com>
2021-03-06 13:57:20 -0600

    formatting

--- scripts/devrc.sh
+++ scripts/devrc.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 cat > $devrc <<-EOF
-source $SCRIPTS/pathmunge.sh
-source $SCRIPTS/filelimit.sh
+source $SCRIPTS/pathmunge.sh;
+source $SCRIPTS/filelimit.sh;
 
-pathmunge "/usr/local/sbin"
+pathmunge "/usr/local/sbin";
 
-alias ls='exa -G'
-alias ll='exa -lh --git --modified --icons'
-alias reload="source $shell_file"
+alias ls='exa -G';
+alias ll='exa -lh --git --modified --icons';
+alias reload="source $shell_file";
 
-alias subl="open -a 'Sublime Text.app'"
-alias atom="open -a 'Atom.app'"
-alias code="open -a 'Visual Studio Code.app'"
+alias subl="open -a 'Sublime Text.app'";
+alias atom="open -a 'Atom.app'";
+alias code="open -a 'Visual Studio Code.app'";
+
+alias watch="npm run watch";
+alias start="npm start";
+alias test="npm run test";
+
+alias pull="git pull";
 
 EOF
