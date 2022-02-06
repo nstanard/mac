cb484b3cdda866b09a0d9926d8fa0ce3c800d23e
Neal Stanard <nstanard@gmail.com>
2022-02-06 12:51:39 -0600

    revert tfplan

--- scripts/devrc.sh
+++ scripts/devrc.sh
@@ -48,7 +48,7 @@ alias code="open -a 'Visual Studio Code.app'"
 # Terraform
 
 alias tfinit="terraform init";
-alias tfplan="terraform plan -out -no-color > tfplan.txt";
+alias tfplan="terraform plan -no-color > tfplan.txt";
 alias tfapply="terraform apply";
 alias tfdestroy="terraform destroy";
 
