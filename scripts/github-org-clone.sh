fd2af8aacd9b7fd22406019931dc9cb1e0fda82c
Neal Stanard <nstanard@users.noreply.github.com>
2022-08-18 11:30:23 -0500

    Update github-org-clone.sh

--- scripts/github-org-clone.sh
+++ scripts/github-org-clone.sh
@@ -1,3 +1,4 @@
+# https://stackoverflow.com/questions/19576742/how-to-clone-all-repos-at-once-from-github
 gh repo list myorgname --limit 1000 | while read -r repo _; do
   gh repo clone "$repo" "$repo"
 done
