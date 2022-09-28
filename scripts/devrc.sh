7d8e9cf221fe7cca2bb9ee8db0ee19267d733213
Neal Stanard <nstanard@users.noreply.github.com>
2022-09-28 12:52:53 -0500

    Update devrc.sh

--- scripts/devrc.sh
+++ scripts/devrc.sh
@@ -274,7 +274,7 @@ if [ -f "'\$UTILS_FOLDER'/google-cloud-sdk/path.zsh.inc" ]; then . "'\$UTILS_FOL
 # The next line enables shell command completion for gcloud.
 if [ -f "'\$UTILS_FOLDER'/google-cloud-sdk/completion.zsh.inc" ]; then . "'\$UTILS_FOLDER'/google-cloud-sdk/completion.zsh.inc"; fi
 
-alias dep:nstanard="(cd $DEV_FOLDER/TheRoutingCompany/core; task infra:deploy:dev ns=nstanard)"
+alias deploy:nstanard="(cd $DEV_FOLDER/TheRoutingCompany/core; task infra:deploy:dev ns=nstanard)"
 alias gen="rai go generate"
 
 EOF
